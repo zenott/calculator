94b85ca8b76c12fc6ab6862722e3ffcd23da98ed
zenott <mblevel@gmail.com>
2019-07-22 10:51:40 +0200

    Add calculate.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 import { main } from '../styles/App.module.css';
 
-function App() {
-  return (
-    <div className={main}>
-      <Display result="result" />
-      <ButtonPanel />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { total: '0', next: null, operation: null };
+  }
+
+  handleClick = buttonName => {
+    this.setState(prevState => calculate(prevState, buttonName));
+  };
+
+  render() {
+    const { total, next } = this.state;
+    return (
+      <div className={main}>
+        <Display result={next || total} />
+        <ButtonPanel handleClick={this.handleClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/logic/calculate.js
@@ -0,0 +1,78 @@
+import operate from './operate';
+
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const operators = ['+', '-', '×', '÷'];
+
+export default function calculate(prevData, buttonName) {
+  let { next, total, operation } = prevData;
+
+  const noError = () => next !== 'Error';
+
+  const maxChar = () => next && next.length === 18;
+
+  if (buttonName === 'AC') {
+    total = '0';
+    next = null;
+    operation = null;
+  }
+
+  if (buttonName === '=' && total && next && operation && noError()) {
+    try {
+      total = operate(total, next, operation);
+      next = null;
+      operation = null;
+    } catch (err) {
+      next = 'Error';
+    }
+  }
+
+  if (buttonName === '.' && noError() && !maxChar()) {
+    if (next && !next.includes('.')) {
+      next += buttonName;
+    } else if (!next) {
+      next = '0.';
+    }
+  }
+
+  if (numbers.includes(buttonName) && next !== '0' && noError() && !maxChar()) {
+    if (next) {
+      next += buttonName;
+    } else {
+      next = buttonName;
+    }
+  }
+
+  if (operators.includes(buttonName) && noError()) {
+    if (total && next && operation) {
+      try {
+        total = operate(total, next, operation);
+        next = null;
+        operation = null;
+      } catch (err) {
+        next = 'Error';
+      }
+    } else {
+      total = next || total;
+    }
+    operation = buttonName;
+    next = null;
+  }
+
+  if (buttonName === '+/-' && noError()) {
+    if (next) {
+      next = operate(next, -1, '×');
+    } else {
+      total = operate(total, -1, '×');
+    }
+  }
+
+  if (buttonName === '%' && noError()) {
+    if (next) {
+      next = operate(next, 100, '÷');
+    } else {
+      total = operate(total, 100, '÷');
+    }
+  }
+
+  return { next, total, operation };
+}
