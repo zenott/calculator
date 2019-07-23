0ba46a95b1b40da07800c9c61d7aa32cb6172c51
zenott <mblevel@gmail.com>
2019-07-23 22:04:11 +0200

    Updates

---
+++ precache-manifest.dfc97667ddacce45f0230315f1b89520.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0aad90e35578ce4c1021a6f3142652",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "d67a9c6864a8adad4ec0",
+    "url": "/calculator/static/css/main.9dcfbddb.chunk.css"
+  },
+  {
+    "revision": "77e80dfbaff811adc770",
+    "url": "/calculator/static/js/2.f38b4958.chunk.js"
+  },
+  {
+    "revision": "d67a9c6864a8adad4ec0",
+    "url": "/calculator/static/js/main.27961949.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
